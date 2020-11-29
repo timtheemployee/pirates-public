cec44d233e98574530286525abaa3b0a69288c93
wxxtfxrmx <timapro21@gmail.com>
2020-11-29 17:52:41 +0300

    Updated fonts, some changes in game over dialog

--- core/src/com/wxxtfxrmx/pirates/uikit/dialog/GameOverDialog.java
+++ core/src/com/wxxtfxrmx/pirates/uikit/dialog/GameOverDialog.java
@@ -1,24 +1,72 @@
 package com.wxxtfxrmx.pirates.uikit.dialog;
 
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 import com.wxxtfxrmx.pirates.screen.levelv2.Constants;
 import com.wxxtfxrmx.pirates.uikit.DefaultSkin;
 import com.wxxtfxrmx.pirates.uikit.HorizontalMargin;
 import com.wxxtfxrmx.pirates.uikit.Icon;
 import com.wxxtfxrmx.pirates.uikit.UiButton;
 import com.wxxtfxrmx.pirates.uikit.UiClickListener;
+import com.wxxtfxrmx.pirates.uikit.UiLabel;
 
 public class GameOverDialog extends UiDialog {
 
     public GameOverDialog() {
         super(new DefaultSkin());
         pad(HorizontalMargin.LARGE.getValue());
+        getTitleTable().defaults().height(Constants.UNIT);
+        getTitleTable().defaults().width(getPrefWidth() - HorizontalMargin.LARGE.getValue() * 2);
         getButtonTable().defaults().height(Constants.UNIT);
         getButtonTable().defaults().width(getPrefWidth() - HorizontalMargin.LARGE.getValue() * 2);
+        createGameOverTitle();
+        createContent();
         createExitButton();
         setModal(true);
         setMovable(false);
         setResizable(false);
+    }
+
+    private void createContent() {
+        UiLabel killedShips = new UiLabel("Убитых кораблей");
+        killedShips.setAlignment(Align.center);
+        UiLabel killedShipsCount = new UiLabel("0");
+        killedShipsCount.setAlignment(Align.center);
+
+        UiLabel coinsEarned = new UiLabel("Монет заработано");
+        coinsEarned.setAlignment(Align.center);
+        UiLabel coinsEarnedCount = new UiLabel("0");
+        coinsEarnedCount.setAlignment(Align.center);
+
+        getContentTable()
+                .add(killedShips)
+                .padLeft(HorizontalMargin.SMALL.getValue())
+                .padRight(HorizontalMargin.SMALL.getValue())
+                .fillX();
+
+        getContentTable()
+                .add(killedShipsCount)
+                .padLeft(HorizontalMargin.SMALL.getValue())
+                .padRight(HorizontalMargin.SMALL.getValue());
+
+        getContentTable().row();
+
+        getContentTable()
+                .add(coinsEarned)
+                .padLeft(HorizontalMargin.SMALL.getValue())
+                .padRight(HorizontalMargin.SMALL.getValue());
+
+        getContentTable()
+                .add(coinsEarnedCount)
+                .padLeft(HorizontalMargin.SMALL.getValue())
+                .padRight(HorizontalMargin.SMALL.getValue());
+    }
+
+    private void createGameOverTitle() {
+        UiLabel label = new UiLabel("Игра окончена");
+        label.setFontScale(1.5f);
+        label.setAlignment(Align.center);
+        getTitleTable().add(label).center();
     }
 
     private void createExitButton() {
--- core/src/com/wxxtfxrmx/pirates/uikit/dialog/PauseDialog.java
+++ core/src/com/wxxtfxrmx/pirates/uikit/dialog/PauseDialog.java
@@ -1,24 +1,37 @@
 package com.wxxtfxrmx.pirates.uikit.dialog;
 
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 import com.wxxtfxrmx.pirates.screen.levelv2.Constants;
 import com.wxxtfxrmx.pirates.uikit.DefaultSkin;
 import com.wxxtfxrmx.pirates.uikit.HorizontalMargin;
 import com.wxxtfxrmx.pirates.uikit.Icon;
 import com.wxxtfxrmx.pirates.uikit.UiButton;
 import com.wxxtfxrmx.pirates.uikit.UiClickListener;
+import com.wxxtfxrmx.pirates.uikit.UiLabel;
 
 public class PauseDialog extends UiDialog {
 
     public PauseDialog() {
         super(new DefaultSkin());
         pad(HorizontalMargin.LARGE.getValue());
+        getTitleTable().defaults().height(Constants.UNIT);
+        getTitleTable().defaults().width(getPrefWidth() - HorizontalMargin.LARGE.getValue() * 2);
         getButtonTable().defaults().height(Constants.UNIT);
         getButtonTable().defaults().width(getPrefWidth() - HorizontalMargin.LARGE.getValue() * 2);
+        createPauseTitle();
         createResumeButton();
         setModal(true);
         setMovable(false);
         setResizable(false);
+    }
+
+    private void createPauseTitle() {
+        UiLabel label = new UiLabel("Пауза");
+        label.setFontScale(1.5f);
+        label.setAlignment(Align.center);
+
+        getTitleTable().add(label);
     }
 
     private void createResumeButton() {
