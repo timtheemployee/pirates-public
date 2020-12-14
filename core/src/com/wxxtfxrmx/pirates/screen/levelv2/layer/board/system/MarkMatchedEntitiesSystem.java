185b566a5e829743f3151456c69aef02a2008a6f
wxxtfxrmx <timapro21@gmail.com>
2020-12-15 00:01:49 +0300

    fixed matched tiles translation

--- core/src/com/wxxtfxrmx/pirates/screen/levelv2/layer/board/system/MarkMatchedEntitiesSystem.java
+++ core/src/com/wxxtfxrmx/pirates/screen/levelv2/layer/board/system/MarkMatchedEntitiesSystem.java
@@ -5,9 +5,14 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.math.Rectangle;
 import com.wxxtfxrmx.pirates.screen.levelv2.Constants;
 import com.wxxtfxrmx.pirates.screen.levelv2.component.BoundsComponent;
 import com.wxxtfxrmx.pirates.screen.levelv2.component.DestinationComponent;
+import com.wxxtfxrmx.pirates.screen.levelv2.layer.battle.component.AiComponent;
+import com.wxxtfxrmx.pirates.screen.levelv2.layer.battle.component.CurrentTurnComponent;
+import com.wxxtfxrmx.pirates.screen.levelv2.layer.battle.component.PlayerComponent;
+import com.wxxtfxrmx.pirates.screen.levelv2.layer.battle.component.TextureSkeletonComponent;
 import com.wxxtfxrmx.pirates.screen.levelv2.layer.board.component.MarkToSendTileComponent;
 import com.wxxtfxrmx.pirates.screen.levelv2.layer.board.component.EmptyPlaceComponent;
 import com.wxxtfxrmx.pirates.screen.levelv2.layer.board.component.TilePickedComponent;
@@ -16,6 +21,12 @@ import com.wxxtfxrmx.pirates.screen.levelv2.layer.board.component.TouchChainComp
 public class MarkMatchedEntitiesSystem extends IteratingSystem {
 
     private final ComponentMapper<BoundsComponent> boundsMapper = ComponentMapper.getFor(BoundsComponent.class);
+    private final ComponentMapper<TextureSkeletonComponent> skeletonMapper = ComponentMapper.getFor(TextureSkeletonComponent.class);
+
+    private final Family currentTurnFamily = Family
+            .all(CurrentTurnComponent.class, TextureSkeletonComponent.class)
+            .get();
+
     private final PooledEngine pooledEngine;
 
     public MarkMatchedEntitiesSystem(PooledEngine pooledEngine) {
@@ -33,9 +44,14 @@ public class MarkMatchedEntitiesSystem extends IteratingSystem {
 
         if (!canCollect()) return;
 
+        Entity currentTurnEntity = getEngine().getEntitiesFor(currentTurnFamily).first();
+
+        TextureSkeletonComponent textureSkeletonComponent = skeletonMapper.get(currentTurnEntity);
+        Rectangle skeletonBounds = textureSkeletonComponent.skeleton.getBounds();
+
         DestinationComponent destinationComponent = pooledEngine.createComponent(DestinationComponent.class);
-        destinationComponent.destination.x = Constants.WIDTH * Constants.UNIT;
-        destinationComponent.destination.y = Constants.MIDDLE_ROUNDED_HEIGHT * Constants.UNIT;
+        destinationComponent.destination.x = skeletonBounds.x + skeletonBounds.width * 0.5f;
+        destinationComponent.destination.y = skeletonBounds.y + skeletonBounds.height * 0.5f;
 
         BoundsComponent boundsComponent = boundsMapper.get(entity);
         boundsComponent.z = 1f;
